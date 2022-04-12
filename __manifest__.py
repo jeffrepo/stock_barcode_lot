35f5ec4f99e8f5a83fbdd81336710138beb9c62b
jeffrepo <jeffer.leo93@gmail.com>
2022-04-11 19:05:48 -0600

    barcode lote

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'stock barcode lot',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 6,
+    'summary': 'stock barcode lot',
+    'description': """
+
+""",
+    'depends': ['stock'],
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
